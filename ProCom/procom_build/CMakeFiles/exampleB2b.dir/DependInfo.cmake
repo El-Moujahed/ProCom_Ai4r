ed3c091c42586d3872ab5a353f0c660f3d1307be
caamic <152409540+caamic@users.noreply.github.com>
2024-01-22 19:00:07 +0000

    Add files via upload
    projet au stade :
    géométrie implémentée (boite, gaz à l'intérieur, detecteur NaI, lame de verre, gap de detection des beta)
    sensitive volume au niveau du scintillateur mais pas encore au gap
    pas encore de données stockées en histogrammes

---
+++ ProCom/procom_build/CMakeFiles/exampleB2b.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/exampleB2b.cc" "CMakeFiles/exampleB2b.dir/exampleB2b.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/exampleB2b.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/ActionInitialization.cc" "CMakeFiles/exampleB2b.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/ActionInitialization.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/ChamberParameterisation.cc" "CMakeFiles/exampleB2b.dir/src/ChamberParameterisation.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/ChamberParameterisation.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/DetectorConstruction.cc" "CMakeFiles/exampleB2b.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/DetectorConstruction.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/DetectorMessenger.cc" "CMakeFiles/exampleB2b.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/DetectorMessenger.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/EventAction.cc" "CMakeFiles/exampleB2b.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/EventAction.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB2b.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/RunAction.cc" "CMakeFiles/exampleB2b.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/RunAction.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/TrackerHit.cc" "CMakeFiles/exampleB2b.dir/src/TrackerHit.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/TrackerHit.cc.o.d"
+  "/home/vgate/Geant4_Example/examples/basic/B2/ProCom/src/TrackerSD.cc" "CMakeFiles/exampleB2b.dir/src/TrackerSD.cc.o" "gcc" "CMakeFiles/exampleB2b.dir/src/TrackerSD.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
